d09bb444f39b46e8de73f56bcb696b6413de7dfe
bcapuano <bcapuano@asu.edu>
2020-06-23 20:34:26 -0700

    - Added relevant research paper to docs - Small refactor + documentation

--- Uchat/client.py
+++ Uchat/client.py
@@ -1,29 +1,40 @@
 import socket
+from sys import stdin
 import selectors
-import typing
+from typing import Dict, Tuple
+
+LISTENING_PORT: int = 49850  # Socket for a Uchat client to listen for incoming connections on
+
+"""
+Represents a client in the p2p network
+Has the ability to send and receive messages to other clients
+"""
 
 
 class Client:
     def __init__(self, other_host):
         self.__listening_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create ipv4 TCP socket
-        self.__listening_port: int = 45673  # Port to listen for incoming messages on
-        self.__host: str = ''  # Binds to any IPv4 address that can route it
-        self.__other_address = (other_host, self.__listening_port)
-        self.__chat_pack = dict()  # Store communicating TCP sockets
+        self.__address = ('', LISTENING_PORT)  # Address that is bindable to all available IPv4 interfaces on system
+        self.__other_address = (other_host, LISTENING_PORT)
 
-        self.__listening_socket.bind((self.__host, self.__listening_port))  # Listen on this address
+        # Store a mapping from a TCP socket's remote address to itself
+        self.__chat_pack = Dict[Tuple[str, int], socket.socket]
+
+        # Set up listening socket to listen on its address
+        self.__listening_socket.bind(self.__address)  # Listen on this address
+        self.__listening_socket.setblocking(False)
         self.__listening_socket.listen()
 
-    def send_message(self, stdin):
-        """
+        print('Client listening on: {}'.format(self.__listening_socket.getsockname()))
 
-        :return:
+    def send_message(self):
+        """
+        Gets a line of input from stdin and sends it to another client
         """
 
         # Get the message to be sent (standard input)
         message = stdin.readline()
 
-        # Send to partner
         # TODO: Figure out how to differentiate multiple clients
         if self.__other_address not in self.__chat_pack:
             # Create a new TCP socket to communicate with other_address
@@ -35,8 +46,7 @@ class Client:
 
     def get_message(self, comm_sock):
         """
-
-        :return:
+        Reads all available bytes from a peer's socket and displays the message to stdout
         """
 
         # Read from listening socket
@@ -46,7 +56,6 @@ class Client:
     def accept_connection(self, listening_sock, selector):
         """
         Accepts a new TCP connection to communicate with another client
-        :return:
         """
 
         new_sock, addr = listening_sock.accept()  # We must have had bound and listened to get here
@@ -65,6 +74,8 @@ class Client:
             self.accept_connection(updated_sock, selector)
         else:
             self.get_message(updated_sock)
+
+    # Getters and Setters
 
     def get_listening_socket(self):
         return self.__listening_socket
