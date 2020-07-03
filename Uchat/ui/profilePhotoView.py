bf4381bc22a57f63c04661387acdef49c713afb7
bcapuano <bcapuano@asu.edu>
2020-07-02 18:26:22 -0700

    Added stub for implementing profile photo view

---
+++ Uchat/ui/profilePhotoView.py
@@ -0,0 +1,7 @@
+from typing import Optional
+from PyQt5.QtWidgets import QWidget
+
+
+class ProfilePhotoView(QWidget):
+    def __init__(self, parent: Optional[QWidget]):
+        super().__init__(parent)
